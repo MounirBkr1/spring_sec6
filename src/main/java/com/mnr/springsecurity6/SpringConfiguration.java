2d760769cda05b982924cc5ceb1713678d554c86
MounirBkr1 <miirbri1@gmail.com>
2024-07-12 17:42:04 -0400

    h2 or db authentication

--- src/main/java/com/mnr/springsecurity6/SpringConfiguration.java
+++ src/main/java/com/mnr/springsecurity6/SpringConfiguration.java
@@ -1,5 +1,6 @@
 package com.mnr.springsecurity6;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -12,41 +13,75 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SpringConfiguration {
 
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     @Order(2147483642)
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> {
-            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)
+                    requests.requestMatchers("/h2-console/**").permitAll()
+                            .anyRequest()).authenticated();
         });
         http.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-       http.formLogin(Customizer.withDefaults());
+        //http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
+        //allow frame from h2 console
+        http.headers(headers ->
+                headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+        http.csrf(csrf-> csrf.disable());
         return (SecurityFilterChain)http.build();
     }
 
 
-    //in memory authentication
+
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user1= User.withUsername("user1")
-                .password("{noop}123") //noop = not coded
+                //.password("{noop}123") //noop = not encode password
+                .password(passwordEncoder().encode("123"))
                 .roles("USER")
                 .build();
 
         UserDetails admin= User.withUsername("admin")
-                .password("{noop}123") //noop = not coded
+                //.password("{noop}123") //noop = not coded
+                .password(passwordEncoder().encode("123"))
                 .roles("ADMIN")
                 .build();
+        //IN MEMORY AUTHENTICATION
+        // return new InMemoryUserDetailsManager(user1,admin);
 
-        return new InMemoryUserDetailsManager(user1,admin);
+
+        //H2 DATABASE AUTHENTICATION
+
+
+
+        JdbcUserDetailsManager userDetailsManager
+                = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+
+
+    }
+
+    //encode password
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
