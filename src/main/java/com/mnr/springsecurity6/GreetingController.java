fc25d7845a522bd74d9ac85dccb45102ee4de8a2
MounirBkr1 <miirbri1@gmail.com>
2024-07-12 12:04:05 -0400

    add role to inMemoryAuthentication

--- src/main/java/com/mnr/springsecurity6/GreetingController.java
+++ src/main/java/com/mnr/springsecurity6/GreetingController.java
@@ -1,5 +1,6 @@
 package com.mnr.springsecurity6;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,4 +12,20 @@ public class GreetingController {
     public String sayHello(){
         return "hello";
     }
+
+    //restricted for user
+    @PreAuthorize("hasRole('USER')") //work before execution of this method
+    @GetMapping("/user")
+    public String userEndpoint(){
+        return "hello, user!";
+    }
+
+    //restricted for admin
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint(){
+        return "hello, admin!";
+    }
+
+
 }
