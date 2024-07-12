9d37f720ea72e627b15e75ba6f6998f63dd29554
MounirBkr1 <miirbri1@gmail.com>
2024-07-12 11:51:56 -0400

    in memory authentication

--- src/main/java/com/mnr/springsecurity6/SpringConfiguration.java
+++ src/main/java/com/mnr/springsecurity6/SpringConfiguration.java
@@ -8,6 +8,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,5 +29,22 @@ public class SpringConfiguration {
        http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
+    }
+
+
+    //in memory authentication
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1= User.withUsername("user1")
+                .password("{noop}123") //noop = not coded
+                .roles("USER")
+                .build();
+
+        UserDetails admin= User.withUsername("admin")
+                .password("{noop}123") //noop = not coded
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,admin);
     }
 }
