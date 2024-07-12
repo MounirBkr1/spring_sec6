6c83e9fe1351658665c53449375b92e254a20bbb
MounirBkr1 <miirbri1@gmail.com>
2024-07-12 11:16:21 -0400

    basic authentication

---
+++ src/main/java/com/mnr/springsecurity6/GreetingController.java
@@ -0,0 +1,14 @@
+package com.mnr.springsecurity6;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+}
